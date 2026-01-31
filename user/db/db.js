433a5c0c90e96d6008976a84c53c02b7e4fe68ce
swamiabhishek45 <abhishekswami1435@gmail.com>
2026-01-31 20:07:33 +0530

    change in mongo uri

--- user/db/db.js
+++ user/db/db.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
-console.log("hi",process.env.MONGO_URI);
-
-
 function connect(){
-    mongoose.connect("mongodb+srv://swamiabhishek45:12345@cluster0.0nsctbe.mongodb.net/user_services").then(()=>{
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
         console.log("MONGODB Connected!!");
     }).catch(()=>{
         console.log("MONGODB Connection error");
