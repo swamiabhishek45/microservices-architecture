42835b71ec45a8f097d2b734c4a69628043fa901
swamiabhishek45 <abhishekswami1435@gmail.com>
2026-02-01 23:33:37 +0530

    implement user login functionality and add blacklist token model

--- user/controller/user.controller.js
+++ user/controller/user.controller.js
@@ -33,4 +33,37 @@ const register = async (req, res) => {
   
 };
 
-module.exports = register;
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel
+            .findOne({ email })
+            .select('+password');
+
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        delete user._doc.password;
+
+        res.cookie('token', token);
+
+        res.send({ token, user });
+
+    } catch (error) {
+
+        res.status(500).json({ message: error.message });
+    }
+
+}
+
+module.exports = [register, login];
